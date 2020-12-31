633eb11918ab692751ee12dd700df0714c050a55
Walid Lezzar <walid.lezzar@adevinta.com>
2020-12-31 15:14:10 +0100

    update Formumat to 0.4.4

--- Formula/jtab.rb
+++ Formula/jtab.rb
@@ -2,20 +2,20 @@ class Jtab < Formula
   desc "Print any json data as a table from the command line"
   homepage "https://github.com/wlezzar/jtab"
   bottle :unneeded
-  version "0.3.0"
+  version "0.4.4"
 
   if OS.mac?
-      url "https://github.com/wlezzar/jtab/releases/download/v0.3.0/jtab-v0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "3eb5568caafab2c217d8da939ad35b4f9f550639e0defb37e28133fb9363eaa6"
+      url "https://github.com/wlezzar/jtab/releases/download/v0.4.4/jtab-v0.4.4-x86_64-apple-darwin.tar.gz"
+      sha256 "9ab2e49239ce2aaeeec7928ed4941f5d721e11d80145adbafade95834af311a1"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/wlezzar/jtab/releases/download/v0.3.0/jtab-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1eb9f4d7911795e8c6a45a3b329b86af4fb3a5db82b21ed40a17fc76281ec1ed"
+      url "https://github.com/wlezzar/jtab/releases/download/v0.4.4/jtab-v0.4.4-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f2143d289a82f111a179698adedbecc5a0a5358e59aa3d6874daeec230e45151"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/wlezzar/jtab/releases/download/v0.3.0/jtab-v0.3.0-arm-unknown-linux-gnueabi.tar.gz"
-        sha256 "4a447a716ebefad57ccb83f1e8c6f16ffe0be7a01a38a48649453738be77e08e"
+        url "https://github.com/wlezzar/jtab/releases/download/v0.4.4/jtab-v0.4.4-arm-unknown-linux-gnueabi.tar.gz"
+        sha256 "49795cbea0c502b712e8452b4614386157761aa6352fb6fed70234ae3145ae03"
       else
       end
     end
