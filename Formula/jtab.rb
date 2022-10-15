30df77fc2cd681a6b1da229261223efe2f2fbc56
Walid Lezzar <walid.lezzar@gmail.com>
2022-10-15 00:09:10 -0400

    update brew formula

--- Formula/jtab.rb
+++ Formula/jtab.rb
@@ -1,26 +1,32 @@
 class Jtab < Formula
   desc "Print any json data as a table from the command line"
   homepage "https://github.com/wlezzar/jtab"
-  version "0.4.4"
+  version "0.5.1"
 
   if OS.mac?
-      url "https://github.com/wlezzar/jtab/releases/download/v0.4.4/jtab-v0.4.4-x86_64-apple-darwin.tar.gz"
-      sha256 "9ab2e49239ce2aaeeec7928ed4941f5d721e11d80145adbafade95834af311a1"
+    if Hardware::CPU.intel?
+      url "https://github.com/wlezzar/jtab/releases/download/v0.5.1/jtab-0.5.1-x86_64-apple-darwin.zip"
+      sha256 "9ba8294485847824e08f654723f841c3cda4b843625de670379fd824e3036fc6"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/wlezzar/jtab/releases/download/v0.5.1/jtab-0.5.1-aarch64-apple-darwin.zip"
+      sha256 "7dec6cf02f39b5e7f09d9945c7a0531d06d0caa6cedd96f58bcdc1269ba168a2"
+    end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/wlezzar/jtab/releases/download/v0.4.4/jtab-v0.4.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f2143d289a82f111a179698adedbecc5a0a5358e59aa3d6874daeec230e45151"
+      url "https://github.com/wlezzar/jtab/releases/download/v0.5.1/jtab-0.5.1-x86_64-unknown-linux-gnu.zip"
+      sha256 "cb2efad8454704ad3f3a3490698fe2d6043d9f0cac87f963e28094e21e3e4889"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/wlezzar/jtab/releases/download/v0.4.4/jtab-v0.4.4-arm-unknown-linux-gnueabi.tar.gz"
-        sha256 "49795cbea0c502b712e8452b4614386157761aa6352fb6fed70234ae3145ae03"
+        url "https://github.com/wlezzar/jtab/releases/download/v0.5.1/jtab-0.5.1-aarch64-unknown-linux-gnu.zip"
+        sha256 "0edd19a008f3b8e3464aef37a68d685b8eb9334902e037d275fc08e2511ce074"
       else
       end
     end
   end
 
   def install
-    bin.install "jtab"
+    bin.install Dir["bin/jtab"]
   end
 end
